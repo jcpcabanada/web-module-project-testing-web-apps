1a07f6580d249289a77d91fb6df710e9c3da3f01
jcpcabanada <jcpcabanada@gmail.com>
2021-12-02 22:20:25 -0800

    yuppp
    Took 1 hour 4 minutes

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -10,32 +10,49 @@ test('renders without errors', ()=>{
 });
 
 test('renders the contact form header', ()=> {
-    
+    render(<ContactForm/>);
+    // console.log(screen);
+    // const { debug } = render(<ContactForm/>);
+    const header1 = screen.queryByText(/Contact Form/i);
+
+    expect(header1).toBeInTheDocument();
+    expect(header1).toHaveTextContent(/contact form/i);
+    expect(header1).toBeTruthy();
+
+
 });
 
 test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {
-    
-});
+    render(<ContactForm/>);
 
+    const firstNameInput = screen.getByLabelText(/first name */i);
+
+    userEvent.type(firstNameInput, "bob");
+
+
+    const errorIndicators =  await screen.findAllByTestId('error');
+    expect(errorIndicators).toHaveLength(1);
+});
+//
 test('renders THREE error messages if user enters no values into any fields.', async () => {
-    
+    render(<ContactForm/>);
 });
-
-test('renders ONE error message if user enters a valid first name and last name but no email.', async () => {
-    
-});
-
-test('renders "email must be a valid email address" if an invalid email is entered', async () => {
-    
-});
-
-test('renders "lastName is a required field" if an last name is not entered and the submit button is clicked', async () => {
-    
-});
-
-test('renders all firstName, lastName and email text when submitted. Does NOT render message if message is not submitted.', async () => {
-    
-});
-
-test('renders all fields text when all fields are submitted.', async () => {
-});
+//
+// test('renders ONE error message if user enters a valid first name and last name but no email.', async () => {
+//
+// });
+//
+// test('renders "email must be a valid email address" if an invalid email is entered', async () => {
+//
+// });
+//
+// test('renders "lastName is a required field" if an last name is not entered and the submit button is clicked', async () => {
+//
+// });
+//
+// test('renders all firstName, lastName and email text when submitted. Does NOT render message if message is not submitted.', async () => {
+//
+// });
+//
+// test('renders all fields text when all fields are submitted.', async () => {
+// });
